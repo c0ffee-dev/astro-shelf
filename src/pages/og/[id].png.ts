5ffc68d6f718119c2bcf67a3e2d3dad590713c8b
cocoa <cocoa@amase.cc>
2026-07-04 00:02:43 +0900

    fix: fix getStaticPaths

--- src/pages/og/[id].png.ts
+++ src/pages/og/[id].png.ts
@@ -1,20 +1,14 @@
-import { readFile } from 'node:fs/promises'
-
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 import { formatDate, generateOGImage } from '../../shared/ogp';
 import { defaultLang, type Lang } from '../../i18n/config';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 export async function getStaticPaths() {
   const blogEntries = await getCollection('blog');
-  return blogEntries.map(async (entry) => {
-    return {
-      params: { id: entry.id },
-      props: { title: entry.data.title, dateString: formatDate(entry.data.createdAt), thumbnail: entry.data.thumbnail },
-    }
-  });
+  return blogEntries.map(entry => ({
+    params: { id: entry.id },
+    props: { title: entry.data.title, dateString: formatDate(entry.data.createdAt), thumbnail: entry.data.thumbnail },
+  }));
 }
 
 export const GET: APIRoute = async ({ props, params }) => {
